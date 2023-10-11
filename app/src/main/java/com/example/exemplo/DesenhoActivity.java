f7a1f4f8575e002289d8e52768501c07fd3910ee
Eugênio <eanassu@gmail.com>
2023-10-11 09:59:19 -0300

    Aula 11/10

---
+++ app/src/main/java/com/example/exemplo/DesenhoActivity.java
@@ -0,0 +1,14 @@
+package com.example.exemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DesenhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desenho);
+    }
+}

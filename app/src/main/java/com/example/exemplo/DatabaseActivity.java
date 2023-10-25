fe4f39affc0c0957c9ed0e9a493769f28cab1596
Eugênio <eanassu@gmail.com>
2023-10-25 10:48:43 -0300

    Aula 25/10 - cadastro e lista

--- app/src/main/java/com/example/exemplo/DatabaseActivity.java
+++ app/src/main/java/com/example/exemplo/DatabaseActivity.java
@@ -2,19 +2,55 @@ package com.example.exemplo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatabaseActivity extends AppCompatActivity {
+
+    private EditText editTextRe;
+    private EditText editTextNome;
+    private EditText editTextDataAdmissao;
+    private EditText editTextSalario;
+    private EditText editTextCargo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        editTextRe = findViewById(R.id.editTextRe);
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextDataAdmissao = findViewById(R.id.editTextDataAdmissao);
+        editTextSalario = findViewById(R.id.editTextSalario);
+        editTextCargo = findViewById(R.id.editTextCargo);
+    }
+
+    public void cadastrar(View view ) {
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+        SimpleDateFormat dateFotmat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataAdmissao;
+        try {
+            dataAdmissao = dateFotmat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e) {
+            dataAdmissao = new Date();
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        String cargo = editTextCargo.getText().toString();
         AppDatabase db = AppDatabase.getInstance(this);
         FuncionarioDao dao = db.funcionarioDao();
-        Funcionario f = new Funcionario(1,"teste",new Date(), 1000, "teste");
+        Funcionario f = new Funcionario(re,nome,dataAdmissao, salario, cargo);
         dao.insert(f);
+
+    }
+
+    public void listar( View view ) {
+        Intent intent = new Intent(this, ListaActivity.class);
+        startActivity(intent);
     }
 }
